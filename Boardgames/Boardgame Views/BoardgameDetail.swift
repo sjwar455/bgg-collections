20a24a7fe6480a91e88280a9b89ef8e952835cf4
Samantha Wareing <samantha.wareing@gmail.com>
2019-11-30 19:36:33 -0700

    improved BoardgameDetail View layout

--- Boardgames/Boardgame Views/BoardgameDetail.swift
+++ Boardgames/Boardgame Views/BoardgameDetail.swift
@@ -25,34 +25,67 @@ struct BoardgameDetail: View {
                   HStack {
                       Text(boardgame.minPlayers + "-" + boardgame.maxPlayers + " players")
                           .font(.subheadline)
+                      
                       Text(boardgame.minPlayTime + "-" + boardgame.maxPlayTime + " mins")
                           .font(.subheadline)
-                      Text(boardgame.minAge + "+")
+                   
+                      Text("ages " + boardgame.minAge + "+")
                           .font(.subheadline)
                           
                       }
+                  .padding(.bottom, 25)
                       
               
                   
         
-        VStack(alignment: .leading) {
+            // boardgame info
+            VStack(alignment: .leading) {
+                
                   Text("Desription")
-                      .font(.title)
-              
+                      .font(.headline)
+                        
                   Text(boardgame.description)
+                    .padding(.bottom, 25)
                   
-              
-                  Text("Categories")
-                      .font(.title)
-             
-                  Text("Mechanics")
-                      .font(.title)
-          
-              }
-        }
-        
-
+               
+                
+                  // boardgame mechanics & categories
+                  HStack(alignment: .top) {
+                    
+                    // boardgame categories
+                    VStack(alignment: .leading) {
+                        Text("Categories")
+                                .font(.headline)
+                
+                    
+                        ForEach(boardgame.categories, id: \.self) { category in
+                                Text(category)
+                                        .font(.body)
+                                } // end ForEach categories
+                    } // end VStack categories
+                        .padding(.trailing, 25)
+                    
             
-    }
+                
+                    // boardgam mechanics
+                    VStack(alignment: .leading) {
+                            Text("Mechanics")
+                                    .font(.headline)
+                
+                            ForEach(boardgame.mechanics, id: \.self) { mechanic in
+                                Text(mechanic)
+                                    .font(.body)
+                            } // end ForEach mechanics
+                    } // end VStack mechanics
+                    
+                } // end HStack of boardgame categories and mechanics
+                
+                } // end VStack info
+                .offset(x: -50)
+            
+               } // end main VStack
+            
+            
+    } // end body
 
-}
+} // end View struct
